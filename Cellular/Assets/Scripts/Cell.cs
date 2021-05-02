358a9c8d6ed46400463b021060c7219137804a72
StephanGuingor <stephan.guingor04@gmail.com>
2021-05-02 14:44:58 -0500

    Added Basic API

---
+++ Cellular/Assets/Scripts/Cell.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public class ConwayCell : MonoBehaviour, ICell
+{
+    private string _ruleSet = "B2/S23";
+    
+    public string GetRuleSet()
+    {
+        return _ruleSet;
+    }
+
+    public void SetRuleSet(string rules)
+    {
+        _ruleSet = rules;
+    }
+}
