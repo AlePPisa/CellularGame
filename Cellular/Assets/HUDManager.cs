e4f50ba4952ac8705407a164be5d371015fab536
AlePPisa <aleppaulon@gmail.com>
2021-05-02 22:06:25 -0500

    Added a main menu, HUD, animations

---
+++ Cellular/Assets/HUDManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HUDManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMenu()
+    {
+        
+    }
+
+    public void RestartLevel()
+    {
+        //TODO: Make this have a fade in fade out
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void StepSimulation()
+    {
+        
+    }
+
+    public void RunSimulation()
+    {
+        
+    }
+}
